fc35f81a341848566484d7219685de1609c2bbdb
Caroline Ramond <caroline.ramond@iconem.com>
2017-11-03 13:31:35 +0100

    customized resizer

--- src/frontend/components/Layout.js
+++ src/frontend/components/Layout.js
@@ -26,12 +26,25 @@ export default class Layout extends React.Component {
 	constructor(props) {
 		super(props);
 		this.logout = this.logout.bind(this);
-		this.handleResizePannel = this.handleResizePannel.bind(this);
+		this.handleDragStarted = this.handleDragStarted.bind(this);
+		this.handleDragFinished = this.handleDragFinished.bind(this);
 		this.switchPreviewMode = this.switchPreviewMode.bind(this);
+		this.state = {
+			isResizing: false
+		}
 	}
 
-	handleResizePannel(props) {
+	handleDragStarted(props) {
+		this.setState({
+			isResizing: true
+		});
+	}
+
+	handleDragFinished(props) {
 		this.props.dispatch(resizeMap());
+		this.setState({
+			isResizing: false
+		});
 	}
 
 	switchPreviewMode(props) {
@@ -43,6 +56,15 @@ export default class Layout extends React.Component {
 	}
 
 	render() {
+		const resizerStyle = {
+			width:"12px", 
+			background:"transparent", 
+			borderLeft: "5px solid #ccc", 
+			cursor: "ew-resize"
+		}
+		const resizerStyleHover = Object.assign({}, resizerStyle, {
+			borderLeft: "5px solid blue"
+		});
 		var authButton;
 		if (this.props.user) {
 			authButton = <Button icon='directions_run' onClick={this.logout}
@@ -84,8 +106,9 @@ export default class Layout extends React.Component {
 		}
 		return <SplitPane split="vertical" defaultSize="70%"
 	    	minSize={750}
-	    	resizerStyle={{border: "2px solid blue"}}
-	    	onDragFinished={this.handleResizePannel}>
+	    	resizerStyle={this.state.isResizing ? resizerStyleHover : resizerStyle}
+	    	onDragStarted={this.handleDragStarted}
+	    	onDragFinished={this.handleDragFinished}>
 	        <div>
 	        	{authButton}
 	        	<div className={mapClass}>
