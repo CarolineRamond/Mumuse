6b91cd746974830d4201cd68753c213b0cc76d24
Caroline Ramond <caroline.ramond@iconem.com>
2017-11-23 15:57:46 +0100

    tests - added world reducer tests => all reducers tests are done

--- src/frontend/modules/world/world.reducer.js
+++ src/frontend/modules/world/world.reducer.js
@@ -1,4 +1,4 @@
-const initialState = {
+export const initialState = {
 	lng: 0,
 	lat: 0,
 	zoom: 0,
@@ -6,33 +6,40 @@ const initialState = {
 	shouldMapResize: false
 }
 
+export const defaultReducer = (state) => {
+	return {
+		...state,
+		shouldMapResize: false
+	};
+}
+
 const worldReducer = (state = initialState, action) => {
 	switch (action.type) {
 	 	case "UPDATE_WORLD_STATE": {
-			return Object.assign({}, state, {
+			return {
+				...state,
 				lat: action.payload.lat,
 				lng: action.payload.lng,
 				zoom: action.payload.zoom,
 				bounds: action.payload.bounds,
 				shouldMapResize: false
-			});
-			break;
+			};
 		}
 		case "RESIZE_MAP": {
-			return Object.assign({}, state, {
+			return {
+				...state,
 				shouldMapResize: true 
-			})
+			};
 		}
 		case "SWITCH_PREVIEW_MODE": {
-			return Object.assign({}, state, {
+			return {
+				...state,
 				previewMode: !state.previewMode,
 				shouldMapResize: true
-			})
+			};
 		}
 		default:
-			return Object.assign({}, state, {
-				shouldMapResize: false
-			});
+			return defaultReducer(state);
 	}
 }
 
