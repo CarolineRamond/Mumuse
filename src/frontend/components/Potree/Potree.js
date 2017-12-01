4a146098e8439dc49c098e1eb816da9bdf053644
Caroline Ramond <caroline.ramond@iconem.com>
2017-12-01 16:56:04 +0100

    fixed bad import (maj)

--- src/frontend/components/Potree/Potree.js
+++ src/frontend/components/Potree/Potree.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
 import potree from '@iconem/iconem-potree';
-import Camera from './camera';
+import Camera from './Camera';
 
 import { selectors } from '../../modules';
 const {
